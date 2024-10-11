8fa4f607aa52ff684e5254de8a6569b274f3856c
Ambjorn97 <turpuhanov.amir@yandex.ru>
2024-10-11 20:54:52 +0300

    задача решена

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,26 +10,48 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        UserService userService = context.getBean(UserService.class);
+//
+        User user1 = new User("Zaur", "Tregulov", "zaur@gmail.com");
+        Car car1 = new Car("Mazda", 6);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+        User user2 = new User("German", "Savastyanov", "kata_noreply@gmail.com");
+        Car car2 = new Car("BMW", 530);
 
-      context.close();
-   }
+        User user3 = new User("Alexandr", "Kabanov", "alexkaban@mail.ru");
+        Car car3 = new Car("Peugeot", 308);
+
+        User user4 = new User("Neil", "Alishev", "neil_alishev@yandex.ru");
+        Car car4 = new Car("Mercedes", 211);
+
+        user1.setCar(car1);
+        user2.setCar(car2);
+        user3.setCar(car3);
+        user4.setCar(car4);
+
+        userService.add(user1);
+        userService.add(user2);
+        userService.add(user3);
+        userService.add(user4);
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("User's car = " + user.getCar());
+            System.out.println();
+        }
+
+        System.out.println(userService.getUserByCar(new Car("Mazda", 6)));
+        System.out.println(userService.getUserByCar(new Car("Peugeot", 308)));
+        System.out.println(userService.getUserByCar(new Car("Mercedes", 211)));
+        System.out.println(userService.getUserByCar(new Car("BMW", 530)));
+    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -1,9 +1,12 @@
 package hiber.service;
 
 import hiber.dao.UserDao;
+import hiber.model.Car;
 import hiber.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -11,19 +14,29 @@ import java.util.List;
 @Service
 public class UserServiceImp implements UserService {
 
-   @Autowired
-   private UserDao userDao;
+    @Autowired
+    private UserDao userDao;
 
-   @Transactional
-   @Override
-   public void add(User user) {
-      userDao.add(user);
-   }
+    @Transactional(rollbackFor = Exception.class, readOnly = false,
+            propagation = Propagation.REQUIRES_NEW)
+    @Override
+    public void add(User user) {
+        userDao.add(user);
+    }
 
-   @Transactional(readOnly = true)
-   @Override
-   public List<User> listUsers() {
-      return userDao.listUsers();
-   }
+    @Transactional(readOnly = true, propagation = Propagation.REQUIRES_NEW,
+            isolation = Isolation.REPEATABLE_READ)
+    @Override
+    public List<User> listUsers() {
+        return userDao.listUsers();
+    }
 
+    @Transactional(readOnly = true, propagation = Propagation.REQUIRES_NEW,
+            isolation = Isolation.REPEATABLE_READ)
+    @Override
+    public User getUserByCar(Car car) {
+        return userDao.getUserByCar(car);
+
+
+    }
 }
